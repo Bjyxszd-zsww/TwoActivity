ec11d030e0eb91dce4bd60fe7b88fb5bd08bc40c
Bjyxszd-zsww <1076361614@qq.com>
2020-09-23 07:35:08 +0800

    Initial commit

---
+++ app/src/main/java/com/example/twoactivity/AnotherActivity.java
@@ -0,0 +1,36 @@
+package com.example.twoactivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class AnotherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+        Button button = (Button)findViewById(R.id.button_second);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = getIntent();
+                String name = intent.getStringExtra("name");
+                Integer age = intent.getIntExtra("age",28);
+                intent.putExtra("result","姓名"+name+"年龄"+age);
+                setResult(0,intent);
+
+                finish();
+            }
+        });
+    }
+}
